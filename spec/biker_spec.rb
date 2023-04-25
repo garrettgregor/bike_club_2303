a8ab5cd9020849eca39aa6392f6fc08f459eb789
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-25 09:23:45 -0600

    Test: biker #learn_terrain

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -4,18 +4,25 @@ RSpec.describe Biker do
   describe "#initialize" do
     it "exists" do
       biker = Biker.new("Kenny", 30)
-      # => #<Biker:0x00007fc62ca41150...>
 
       expect(biker.name).to eq("Kenny")
       expect(biker.max_distance).to eq(30)
       expect(biker.rides).to eq({})
       expect(biker.acceptable_terrain).to eq([])
-      # biker.learn_terrain!(:gravel)
+    end
+  end
 
-      # biker.learn_terrain!(:hills)
+  describe "#learn_terrain!" do
+    it "adds terrain to list of acceptable terrain" do
+      biker = Biker.new("Kenny", 30)
+      # => #<Biker:0x00007fc62ca41150...>
 
-      # biker.acceptable_terrain
-      # # => [:gravel, :hills]
+      expect(biker.acceptable_terrain).to eq([])
+
+      biker.learn_terrain!(:gravel)
+      biker.learn_terrain!(:hills)
+
+      expect(biker.acceptable_terrain).to eq([:gravel, :hills])
     end
   end
 end
