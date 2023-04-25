11e2c22cde8a75d9fbf5fb1e43fd0f96806fa270
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-25 09:41:18 -0600

    Test: biker #personal_record

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -28,6 +28,31 @@ RSpec.describe Biker do
   describe "#log_ride" do
     it "logs ride to ride list" do
       biker = Biker.new("Kenny", 30)
+      biker.learn_terrain!(:gravel)
+      biker.learn_terrain!(:hills)
+
+      expect(biker.acceptable_terrain).to eq([:gravel, :hills])
+
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+
+      biker.log_ride(ride1, 92.5)
+      biker.log_ride(ride1, 91.1)
+      biker.log_ride(ride2, 60.9)
+      biker.log_ride(ride2, 61.6)
+
+      expected = {
+        ride1 => [92.5, 91.1],
+        ride2 => [60.9, 61.6]
+      }
+
+      expect(biker.rides).to eq(expected)
+    end
+  end
+
+  describe "#personal_record" do
+    it "finds the riders personal record for the given ride" do
+      biker = Biker.new("Kenny", 30)
       # => #<Biker:0x00007fc62ca41150...>
       biker.learn_terrain!(:gravel)
       biker.learn_terrain!(:hills)
@@ -51,6 +76,8 @@ RSpec.describe Biker do
       }
 
       expect(biker.rides).to eq(expected)
+      expect(biker.personal_record(ride1)).to eq(91.1)
+      expect(biker.personal_record(ride2)).to eq(60.9)
     end
   end
 end
