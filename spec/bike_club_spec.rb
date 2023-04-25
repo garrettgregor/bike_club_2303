392cf7af9729b6ce2f412d148bed717525e299f7
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-25 11:11:22 -0600

    Test: BikeClub #bikers_eligible

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -97,6 +97,33 @@ RSpec.describe BikeClub do
       expect(bike_club.best_time(ride1)).to eq(biker1)
     end
   end
+
+  describe "#bikers_eligible(ride)" do
+    it "finds the bikers who are eligible for a given ride" do
+      bike_club = BikeClub.new("Boulder BC")
+
+      biker1 = Biker.new("Kenny", 30)
+      biker1.learn_terrain!(:gravel)
+      biker1.learn_terrain!(:hills)
+
+      biker2 = Biker.new("Athena", 15)
+      biker2.learn_terrain!(:gravel)
+      biker2.learn_terrain!(:hills)
+
+      biker3 = Biker.new("Michael", 30)
+      biker3.learn_terrain!(:gravel)
+      biker3.learn_terrain!(:hills)
+
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+
+      bike_club.add_biker(biker1)
+      bike_club.add_biker(biker2)
+      bike_club.add_biker(biker3)
+
+      expect(bike_club.bikers_eligible(ride2)).to eq([biker1, biker3])
+    end
+  end
 end
 
 ## Test Setup
