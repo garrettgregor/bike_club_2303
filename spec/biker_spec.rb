e9f470011c121f77ba6b6ee7791d4d2121fed67b
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-25 09:29:03 -0600

    Test: biker #log_ride

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -15,7 +15,6 @@ RSpec.describe Biker do
   describe "#learn_terrain!" do
     it "adds terrain to list of acceptable terrain" do
       biker = Biker.new("Kenny", 30)
-      # => #<Biker:0x00007fc62ca41150...>
 
       expect(biker.acceptable_terrain).to eq([])
 
@@ -23,6 +22,35 @@ RSpec.describe Biker do
       biker.learn_terrain!(:hills)
 
       expect(biker.acceptable_terrain).to eq([:gravel, :hills])
+    end
+  end
+
+  describe "#log_ride" do
+    it "logs ride to ride list" do
+      biker = Biker.new("Kenny", 30)
+      # => #<Biker:0x00007fc62ca41150...>
+      biker.learn_terrain!(:gravel)
+      biker.learn_terrain!(:hills)
+
+      expect(biker.acceptable_terrain).to eq([:gravel, :hills])
+
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      # => #<Ride:0x00007fc62ca32a10...>
+
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+      # => #<Ride:0x00007fc62cb42ba8...>
+
+      biker.log_ride(ride1, 92.5)
+      biker.log_ride(ride1, 91.1)
+      biker.log_ride(ride2, 60.9)
+      biker.log_ride(ride2, 61.6)
+
+      expected = {
+        ride1 => [92.5, 91.1],
+        ride2 => [60.9, 61.6]
+      }
+
+      expect(biker.rides).to eq(expected)
     end
   end
 end
